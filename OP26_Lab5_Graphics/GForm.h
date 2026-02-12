14a9df06d213f7c9ba6f6a45e6d9f4b030bbb4f2
TokarIvan141 <tokar.ivan.a@chnu.edu.ua>
2026-02-12 03:55:35 +0200

    fix: UI layout and input toggle logic

--- OP26_Lab5_Graphics/GForm.h
+++ OP26_Lab5_Graphics/GForm.h
@@ -4,10 +4,7 @@
 namespace OP26_Lab5_Graphics {
 
 	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
 	using namespace System::Windows::Forms;
-	using namespace System::Data;
 	using namespace System::Drawing;
 
 	public ref class GForm : public System::Windows::Forms::Form
@@ -27,173 +24,163 @@ namespace OP26_Lab5_Graphics {
 			}
 		}
 
-	private: System::Windows::Forms::PictureBox^ pictureBox1;
-	private: System::Windows::Forms::Button^ btnDraw;
-	private: System::Windows::Forms::RadioButton^ rbTask1;
-	private: System::Windows::Forms::RadioButton^ rbTask2;
-	private: System::Windows::Forms::GroupBox^ grpBoxChoice;
-	private: System::ComponentModel::Container^ components;
+	private:
+		System::Windows::Forms::PictureBox^ pictureBox1;
+		System::Windows::Forms::Button^ btnDraw;
+		System::Windows::Forms::RadioButton^ rbTask1;
+		System::Windows::Forms::RadioButton^ rbTask2;
+		System::Windows::Forms::GroupBox^ grpBoxChoice;
+		System::Windows::Forms::GroupBox^ grpParams;
+		System::Windows::Forms::NumericUpDown^ nudP1x; System::Windows::Forms::NumericUpDown^ nudP1y;
+		System::Windows::Forms::NumericUpDown^ nudP2x; System::Windows::Forms::NumericUpDown^ nudP2y;
+		System::Windows::Forms::NumericUpDown^ nudV1x; System::Windows::Forms::NumericUpDown^ nudV1y;
+		System::Windows::Forms::NumericUpDown^ nudV2x; System::Windows::Forms::NumericUpDown^ nudV2y;
+		System::Windows::Forms::NumericUpDown^ nudDepth;
+		System::ComponentModel::Container^ components;
 
-		   void InitializeComponent(void)
-		   {
-			   this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			   this->btnDraw = (gcnew System::Windows::Forms::Button());
-			   this->rbTask1 = (gcnew System::Windows::Forms::RadioButton());
-			   this->rbTask2 = (gcnew System::Windows::Forms::RadioButton());
-			   this->grpBoxChoice = (gcnew System::Windows::Forms::GroupBox());
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
-			   this->grpBoxChoice->SuspendLayout();
-			   this->SuspendLayout();
+		void AddInput(String^ labelText, System::Windows::Forms::NumericUpDown^% nud, int x, int y, float defVal) {
+			Label^ lbl = gcnew Label();
+			lbl->Text = labelText;
+			lbl->Location = System::Drawing::Point(x, y);
+			lbl->AutoSize = true;
+			nud = gcnew NumericUpDown();
+			nud->Location = System::Drawing::Point(x + 45, y - 3);
+			nud->Size = System::Drawing::Size(70, 25);
+			nud->Minimum = -2000;
+			nud->Maximum = 2000;
+			nud->Value = (Decimal)defVal;
+			this->grpParams->Controls->Add(lbl);
+			this->grpParams->Controls->Add(nud);
+		}
 
-			   this->pictureBox1->BackColor = System::Drawing::Color::White;
-			   this->pictureBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			   this->pictureBox1->Location = System::Drawing::Point(20, 130);
-			   this->pictureBox1->Name = L"pictureBox1";
-			   this->pictureBox1->Size = System::Drawing::Size(760, 430);
-			   this->pictureBox1->TabIndex = 0;
-			   this->pictureBox1->TabStop = false;
+		void InitializeComponent(void)
+		{
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->btnDraw = (gcnew System::Windows::Forms::Button());
+			this->rbTask1 = (gcnew System::Windows::Forms::RadioButton());
+			this->rbTask2 = (gcnew System::Windows::Forms::RadioButton());
+			this->grpBoxChoice = (gcnew System::Windows::Forms::GroupBox());
+			this->grpParams = (gcnew System::Windows::Forms::GroupBox());
+			this->SuspendLayout();
 
-			   this->btnDraw->BackColor = System::Drawing::Color::DodgerBlue;
-			   this->btnDraw->Cursor = System::Windows::Forms::Cursors::Hand;
-			   this->btnDraw->FlatAppearance->BorderSize = 0;
-			   this->btnDraw->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			   this->btnDraw->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
-			   this->btnDraw->ForeColor = System::Drawing::Color::White;
-			   this->btnDraw->Location = System::Drawing::Point(350, 40);
-			   this->btnDraw->Name = L"btnDraw";
-			   this->btnDraw->Size = System::Drawing::Size(180, 50);
-			   this->btnDraw->TabIndex = 1;
-			   this->btnDraw->Text = L"ПОБУДУВАТИ";
-			   this->btnDraw->UseVisualStyleBackColor = false;
-			   this->btnDraw->Click += gcnew System::EventHandler(this, &GForm::btnDraw_Click);
+			this->pictureBox1->BackColor = System::Drawing::Color::White;
+			this->pictureBox1->Location = System::Drawing::Point(330, 20);
+			this->pictureBox1->Size = System::Drawing::Size(730, 540);
 
-			   this->grpBoxChoice->Controls->Add(this->rbTask1);
-			   this->grpBoxChoice->Controls->Add(this->rbTask2);
-			   this->grpBoxChoice->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
-			   this->grpBoxChoice->Location = System::Drawing::Point(20, 20);
-			   this->grpBoxChoice->Name = L"grpBoxChoice";
-			   this->grpBoxChoice->Size = System::Drawing::Size(300, 90);
-			   this->grpBoxChoice->TabIndex = 2;
-			   this->grpBoxChoice->TabStop = false;
-			   this->grpBoxChoice->Text = L" Вибір алгоритму ";
+			this->grpBoxChoice->Text = L" Алгоритм ";
+			this->grpBoxChoice->Location = System::Drawing::Point(20, 20);
+			this->grpBoxChoice->Size = System::Drawing::Size(290, 90);
+			this->rbTask1->Checked = true;
+			this->rbTask1->Text = L"Сплайн Ерміта";
+			this->rbTask1->Location = System::Drawing::Point(20, 30);
+			this->rbTask1->CheckedChanged += gcnew EventHandler(this, &GForm::OnModeChanged);
+			this->rbTask2->Text = L"Фрактал Коха";
+			this->rbTask2->Location = System::Drawing::Point(20, 55);
+			this->grpBoxChoice->Controls->Add(this->rbTask1);
+			this->grpBoxChoice->Controls->Add(this->rbTask2);
 
-			   this->rbTask1->AutoSize = true;
-			   this->rbTask1->Checked = true;
-			   this->rbTask1->Location = System::Drawing::Point(20, 30);
-			   this->rbTask1->Name = L"rbTask1";
-			   this->rbTask1->Size = System::Drawing::Size(186, 24);
-			   this->rbTask1->TabIndex = 0;
-			   this->rbTask1->TabStop = true;
-			   this->rbTask1->Text = L"Сплайн Ерміта";
-			   this->rbTask1->UseVisualStyleBackColor = true;
+			this->grpParams->Text = L" Параметри ";
+			this->grpParams->Location = System::Drawing::Point(20, 120);
+			this->grpParams->Size = System::Drawing::Size(290, 280);
 
-			   this->rbTask2->AutoSize = true;
-			   this->rbTask2->Location = System::Drawing::Point(20, 55);
-			   this->rbTask2->Name = L"rbTask2";
-			   this->rbTask2->Size = System::Drawing::Size(176, 24);
-			   this->rbTask2->TabIndex = 1;
-			   this->rbTask2->Text = L"Фрактал Коха (Квадрат)";
-			   this->rbTask2->UseVisualStyleBackColor = true;
+			AddInput(L"P1.x", nudP1x, 15, 40, 100.0f); AddInput(L"P1.y", nudP1y, 150, 40, 215.0f);
+			AddInput(L"P2.x", nudP2x, 15, 85, 600.0f); AddInput(L"P2.y", nudP2y, 150, 85, 215.0f);
+			AddInput(L"V1.x", nudV1x, 15, 145, 200.0f); AddInput(L"V1.y", nudV1y, 150, 145, -500.0f);
+			AddInput(L"V2.x", nudV2x, 15, 190, 200.0f); AddInput(L"V2.y", nudV2y, 150, 190, -500.0f);
 
-			   this->AutoScaleDimensions = System::Drawing::SizeF(8, 20);
-			   this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			   this->BackColor = System::Drawing::Color::FromArgb(245, 247, 250);
-			   this->ClientSize = System::Drawing::Size(800, 580);
-			   this->Controls->Add(this->grpBoxChoice);
-			   this->Controls->Add(this->btnDraw);
-			   this->Controls->Add(this->pictureBox1);
-			   this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
-			   this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
-			   this->MaximizeBox = false;
-			   this->Name = L"GForm";
-			   this->Text = L"Графічний редактор";
-			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
-			   this->grpBoxChoice->ResumeLayout(false);
-			   this->grpBoxChoice->PerformLayout();
-			   this->ResumeLayout(false);
-		   }
+			Label^ lblD = gcnew Label(); lblD->Text = L"Глибина:"; lblD->Location = Point(15, 240);
+			lblD->AutoSize = true;
+			nudDepth = gcnew NumericUpDown();
+			nudDepth->Location = Point(100, 237);
+			nudDepth->Maximum = 6;
+			nudDepth->Value = 4;
+			nudDepth->Enabled = false;
+			this->grpParams->Controls->Add(lblD);
+			this->grpParams->Controls->Add(nudDepth);
 
-		   void DrawHermiteSpline(Graphics^ g, Pen^ pen, PointF P1, PointF P2, PointF V1, PointF V2)
-		   {
-			   float step = 0.001f;
-			   float oldX = P1.X;
-			   float oldY = P1.Y;
+			this->btnDraw->BackColor = System::Drawing::Color::DodgerBlue;
+			this->btnDraw->FlatStyle = FlatStyle::Flat;
+			this->btnDraw->ForeColor = Color::White;
+			this->btnDraw->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, FontStyle::Bold));
+			this->btnDraw->Location = System::Drawing::Point(20, 510);
+			this->btnDraw->Size = System::Drawing::Size(290, 50);
+			this->btnDraw->Text = L"ПОБУДУВАТИ";
+			this->btnDraw->Click += gcnew EventHandler(this, &GForm::btnDraw_Click);
 
-			   for (float t = 0; t <= 1.0; t += step)
-			   {
-				   float h1 = 2 * pow(t, 3) - 3 * pow(t, 2) + 1;
-				   float h2 = -2 * pow(t, 3) + 3 * pow(t, 2);
-				   float h3 = pow(t, 3) - 2 * pow(t, 2) + t;
-				   float h4 = pow(t, 3) - pow(t, 2);
+			this->ClientSize = System::Drawing::Size(1080, 580);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->grpBoxChoice);
+			this->Controls->Add(this->grpParams);
+			this->Controls->Add(this->btnDraw);
+			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
+			this->Text = L"Графіка: Лабораторна №5";
+			this->ResumeLayout(false);
+		}
 
-				   float x = h1 * P1.X + h2 * P2.X + h3 * V1.X + h4 * V2.X;
-				   float y = h1 * P1.Y + h2 * P2.Y + h3 * V1.Y + h4 * V2.Y;
+		void OnModeChanged(Object^ s, EventArgs^ e) {
+			bool h = rbTask1->Checked;
+			nudP1x->Enabled = h;
+			nudP1y->Enabled = h;
+			nudP2x->Enabled = h;
+			nudP2y->Enabled = h;
+			nudV1x->Enabled = h;
+			nudV1y->Enabled = h;
+			nudV2x->Enabled = h;
+			nudV2y->Enabled = h;
+			nudDepth->Enabled = !h;
+		}
 
-				   g->DrawLine(pen, oldX, oldY, x, y);
-				   oldX = x;
-				   oldY = y;
-			   }
-		   }
+		void DrawHermiteSpline(Graphics^ g, Pen^ pen, PointF P1, PointF P2, PointF V1, PointF V2)
+		{
+			float step = 0.001f;
+			float oldX = P1.X, oldY = P1.Y;
+			for (float t = 0.0f; t <= 1.0f; t += step) {
+				float t2 = t * t, t3 = t2 * t;
+				float h1 = 2 * t3 - 3 * t2 + 1, h2 = -2 * t3 + 3 * t2, h3 = t3 - 2 * t2 + t, h4 = t3 - t2;
+				float x = h1 * P1.X + h2 * P2.X + h3 * V1.X + h4 * V2.X;
+				float y = h1 * P1.Y + h2 * P2.Y + h3 * V1.Y + h4 * V2.Y;
+				g->DrawLine(pen, oldX, oldY, x, y);
+				oldX = x; oldY = y;
+			}
+		}
 
-		   void DrawKochLine(Graphics^ g, Pen^ pen, PointF p1, PointF p2, int depth)
-		   {
-			   if (depth == 0) {
-				   g->DrawLine(pen, p1, p2);
-			   }
-			   else {
-				   float dx = p2.X - p1.X;
-				   float dy = p2.Y - p1.Y;
-
-				   PointF pA(p1.X + dx / 3.0f, p1.Y + dy / 3.0f);
-				   PointF pB(p1.X + 2 * dx / 3.0f, p1.Y + 2 * dy / 3.0f);
-
-				   double angle = -System::Math::PI / 2.0;
-
-				   float xx = pA.X - pB.X;
-				   float yy = pA.Y - pB.Y;
-
-				   float pCx = pB.X + xx * (float)cos(angle) - yy * (float)sin(angle);
-				   float pCy = pB.Y + xx * (float)sin(angle) + yy * (float)cos(angle);
-				   PointF pC(pCx, pCy);
-
-				   DrawKochLine(g, pen, p1, pA, depth - 1);
-				   DrawKochLine(g, pen, pA, pC, depth - 1);
-				   DrawKochLine(g, pen, pC, pB, depth - 1);
-				   DrawKochLine(g, pen, pB, p2, depth - 1);
-			   }
-		   }
+		void DrawKochLine(Graphics^ g, Pen^ pen, PointF p1, PointF p2, int depth)
+		{
+			if (depth == 0) g->DrawLine(pen, p1, p2);
+			else {
+				float dx = p2.X - p1.X, dy = p2.Y - p1.Y;
+				PointF pA(p1.X + dx / 3.0f, p1.Y + dy / 3.0f), pB(p1.X + 2 * dx / 3.0f, p1.Y + 2 * dy / 3.0f);
+				double angle = -System::Math::PI / 2.0;
+				float xx = pA.X - pB.X, yy = pA.Y - pB.Y;
+				PointF pC(pB.X + xx * (float)cos(angle) - yy * (float)sin(angle), pB.Y + xx * (float)sin(angle) + yy * (float)cos(angle));
+				DrawKochLine(g, pen, p1, pA, depth - 1); DrawKochLine(g, pen, pA, pC, depth - 1);
+				DrawKochLine(g, pen, pC, pB, depth - 1); DrawKochLine(g, pen, pB, p2, depth - 1);
+			}
+		}
 
 	private: System::Void btnDraw_Click(System::Object^ sender, System::EventArgs^ e) {
-		if (pictureBox1->Image == nullptr) {
-			pictureBox1->Image = gcnew Bitmap(pictureBox1->Width, pictureBox1->Height);
-		}
+		if (pictureBox1->Image == nullptr) pictureBox1->Image = gcnew Bitmap(pictureBox1->Width, pictureBox1->Height);
 		Bitmap^ bmp = (Bitmap^)pictureBox1->Image;
 		Graphics^ g = Graphics::FromImage(bmp);
 		g->SmoothingMode = System::Drawing::Drawing2D::SmoothingMode::AntiAlias;
 		g->Clear(Color::White);
 
-		if (rbTask1->Checked)
-		{
-			Pen^ penHermite = gcnew Pen(Color::FromArgb(0, 102, 204), 2.5f);
-			PointF P1(100.0f, 215.0f), P2(660.0f, 215.0f);
-			PointF V1(200.0f, -500.0f), V2(200.0f, -500.0f);
-
-			DrawHermiteSpline(g, penHermite, P1, P2, V1, V2);
+		if (rbTask1->Checked) {
+			Pen^ p = gcnew Pen(Color::DodgerBlue, 2.5f);
+			PointF P1((float)nudP1x->Value, (float)nudP1y->Value);
+			PointF P2((float)nudP2x->Value, (float)nudP2y->Value);
+			PointF V1((float)nudV1x->Value, (float)nudV1y->Value);
+			PointF V2((float)nudV2x->Value, (float)nudV2y->Value);
+			DrawHermiteSpline(g, p, P1, P2, V1, V2);
 			g->FillEllipse(Brushes::Tomato, P1.X - 5.0f, P1.Y - 5.0f, 10.0f, 10.0f);
 			g->FillEllipse(Brushes::Tomato, P2.X - 5.0f, P2.Y - 5.0f, 10.0f, 10.0f);
 		}
-		else
-		{
-			Pen^ penFractal = gcnew Pen(Color::FromArgb(45, 45, 45), 1.2f);
-			int depth = 4;
-			PointF A(280.0f, 115.0f), B(480.0f, 115.0f), C(480.0f, 315.0f), D(280.0f, 315.0f);
-
-			DrawKochLine(g, penFractal, A, B, depth);
-			DrawKochLine(g, penFractal, B, C, depth);
-			DrawKochLine(g, penFractal, C, D, depth);
-			DrawKochLine(g, penFractal, D, A, depth);
+		else {
+			Pen^ p = gcnew Pen(Color::Black, 1.0f);
+			int d = (int)nudDepth->Value;
+			PointF A(250.0f, 150.0f), B(450.0f, 150.0f), C(450.0f, 350.0f), D(250.0f, 350.0f);
+			DrawKochLine(g, p, A, B, d); DrawKochLine(g, p, B, C, d); DrawKochLine(g, p, C, D, d); DrawKochLine(g, p, D, A, d);
 		}
-
 		pictureBox1->Image = bmp;
 		delete g;
 	}
